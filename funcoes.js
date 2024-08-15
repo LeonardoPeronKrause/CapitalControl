41aba2967af7ed0401a659f1f09fe4ef9af6f562
Leonardo Peron Krause <102702934+LeonardoPeronKrause@users.noreply.github.com>
2024-08-15 10:31:57 -0300

    Add files via upload
    Atualização no arquivo de funcoes

--- funcoes.js
+++ funcoes.js
@@ -31,7 +31,7 @@ const exibirMenu = function () {
 const cadastrarAtivo = function() {
     rl.question('1. Renda Variável\n2. Renda Fixa\nQual a categoria de ativo que você deseja cadastrar?', function(opcao) {
         if (opcao === '1') {
-            rl.question('1. Ação Brasileira\n2. Fundo Imobiliário\n3. Ação Americana (BDR)\n4. Criptoativos\n Qual dos segmentos você deseja cadastrar?', function(opcao) {
+            rl.question('1. Ação Brasileira\n2. Fundo Imobiliário\n3. Ação Americana (BDR)\n4. Criptoativos\n Qual dos segmentos você deseja cadastrar? ', function(opcao) {
                 if (opcao === '1') {
                     cadastrarAcaoBrasileira();
                 } else if (opcao === '2') {
@@ -95,11 +95,45 @@ const sair = function() {
 };
 
 const cadastrarAcaoBrasileira = function() {
-    console.log('Função cadastrar ação brasileira ainda não implementada.');
-    exibirMenu();
+    rl.question('Nome da ação: ', function(nome) {
+        rl.question('Código da ação: (Ex.: PETR3) ',function(ticker) {
+            rl.question('Preço médio de compra ação: ', function(pm) {
+                rl.question('Setor: ', function(setor) {
+                    rl.question('Quantidade de ações: ', function(quantidade) {
+                        if (!nome || !ticker || !pm || !setor || !quantidade) {
+                            console.log('Todos os campos precisam ser preenchidos');
+                            return exibirMenu();
+                        }
+
+                        pm = pm.replace(',', '.');
+
+                        const precoMedio = parseFloat(pm);
+                        const quantidadeAcoes = parseInt(quantidade);
+
+                        if (isNaN(precoMedio) || isNaN(quantidadeAcoes)) {
+                            console.log('Preço médio ou quantidade de ações inválidos.')
+                            return exibirMenu();
+                        }
+
+                        const query = `INSERT INTO acoes (nome, ticker, pm, setor, quantidade) VALUES (?, ?, ?, ?, ?)`;
+                        const values = [nome, ticker, precoMedio, setor, quantidadeAcoes]; 
+
+                        db.query(query, values, (err) => {
+                            if (err) {
+                                console.log('Erro ao cadastrar dados da ação: ', err.message);
+                            } else {
+                                console.log(`A ação ${nome} (${ticker}) foi cadastrada com sucesso!`);
+                            }
+                            exibirMenu();
+                        });
+                    });
+                });
+            });
+        });
+    });
 }
 
-const cadastrarFundoImobiliario = function(){
+const cadastrarFundoImobiliario = function() {
     console.log('Função cadastrar fundo imobiliario ainda não implementada.');
     exibirMenu();
 }
