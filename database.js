509582633cddb969173757e510a7fce552fe912b
Leonardo Peron Krause <102702934+LeonardoPeronKrause@users.noreply.github.com>
2024-08-27 12:26:10 -0300

    comentários

--- database.js
+++ database.js
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
 const { Pool } = require('pg');
 
+// Cria uma nova instância do Pool p gerenciar as conexoes c o db 
 const pool = new Pool ({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'leonardo',
-    password: '1234',
-    port: 5432
+    user: 'postgres',       // nome de usuário
+    host: 'localhost',      // endereço do servidor do db
+    database: 'leonardo',   // nome do db
+    password: '1234',       // senha do usuário do db
+    port: 5432              // porta do servidor 
 });
 
 // Função para realizar consulta ao banco de dados
@@ -28,41 +28,8 @@ const executarQuery = function(query, values, mensagemSucesso, callback) {
     });
 };
 
+// Exporta as funções query e executarQuery
 module.exports = {
     query,
     executarQuery
-=======
-const { Pool } = require('pg');
-
-const pool = new Pool ({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'leonardo',
-    password: '1234',
-    port: 5432
-});
-
-// Função para realizar consulta ao banco de dados
-const query = (text, params) => pool.query(text, params);
-
-// Função para executar uma consulta SQL no banco de dados
-const executarQuery = function(query, values, mensagemSucesso, callback) {
-    //Executa a consulta SQL com os valores fornecidos
-    db.query(query, values, (err) => {
-        // Se ocorrer um erro, exibe uma mensagem de erro
-        if (err) {
-            console.log('Erro ao cadastra dados: ', err.message);
-        } else {
-            // Se a consulta for bem-sucedida, exibe a mensagem de sucesso
-            console.log(mensagemSucesso);
-        }
-        // Chama o callback, se fornecido (geralmente para o fluxo)
-        if (callback) callback();
-    });
-};
-
-module.exports = {
-    query,
-    executarQuery
->>>>>>> 95948d1ff2e2edca0caabe406ab39b442224d489
-};
+}
--- visualizacao.js
+++ visualizacao.js
@@ -1,235 +1,124 @@
-<<<<<<< HEAD
 'use strict';
 
-const db = require('./database.js');
-const rl = require('./readline.js');
+// Importa os módulos necessários
+const db = require('./database.js');    // Módulo p interagir c o db
+const rl = require('./readline.js');    // Módulo p manipulação da entrada de dados via terminal
+const { exibirMenu } = require('./apps.js') // Importa a função p exibir o menu0
 
+
+// Função para visualizar os ativos de investimeno
 const verAtivos = function() {
+    // Pergunta ao usuário ql tipo de investimento deseja ver
     rl.question('1. Renda Variável\n2. Renda Fixa\nQual investimento você quer ver: ', function(opcao) {
         switch (opcao) {
             case '1': // Renda Variável
+            // Pergunta qual ativo especifico o usuario quer ver
                 rl.question('1. Ação Brasileira\n2. Fundo Imobiliário\n3. Ação Americana (BDR)\n4. Criptoativos\nEscolha o ativo que você deseja visualizar: ', function(opcao) {
                     switch (opcao) {
                         case '1':
-                            const queryAcao = 'SELECT * FROM acoes';
+                            const queryAcao = 'SELECT * FROM acoes'; // Consulta para ações brasileiras
                             db.query(queryAcao, (err, results) => {
                                 if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message);
+                                    console.log('Erro ao puxar dados: ', err.message); // Exibe erro (se houver)
                                 } else {
-                                    console.log('Dados das ações:', results);
+                                    console.log('Dados das ações:', results); // Exibe os resultado das ações
                                 }
-                                exibirMenu();
+                                exibirMenu(); // Exibe o menu apóes consulta
                             });
                             break;
                         case '2':
-                            const queryFii = 'SELECT * FROM fii';
+                            const queryFii = 'SELECT * FROM fii'; // Consulta para fundos imobiliários
                             db.query(queryFii, (err, results) => {
                                 if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message)
+                                    console.log('Erro ao puxar dados: ', err.message) // Erro (se houver)
                                 } else {
-                                    console.log('Dados dos Fundos Imobiliários:', results);
+                                    console.log('Dados dos Fundos Imobiliários:', results); // Exibe os resultados dos FIIs
                                 }
-                                exibirMenu();
+                                exibirMenu(); // Exibe o menu após a consulta
                             });
                             break;
                         case '3':
-                            const queryBdr = 'SELECT * FROM bdr';
+                            const queryBdr = 'SELECT * FROM bdr'; // Consulta para BDRs
                             db.query(queryBdr, (err, results) => {
                                 if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message);
+                                    console.log('Erro ao puxar dados: ', err.message); // Erro se houver
                                 } else {
-                                    console.log('Dados das Ações americanas: ', results);
+                                    console.log('Dados das Ações americanas: ', results); // Exibe os resultados das ações americanas
                                 }
-                                exibirMenu();
+                                exibirMenu(); // Menu após a consulta
                             });
                             break;
                         case '4':
-                            const queryCripto = 'SELECT * FROM cripto';
+                            const queryCripto = 'SELECT * FROM cripto'; // Consulta para criptomoedas
                             db.query(queryCripto, (err, results) => {
                                 if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message);
+                                    console.log('Erro ao puxar dados: ', err.message); // Erro (se houver)
                                 } else {
-                                    console.log('Dados das Criptomoedas: ', results);
+                                    console.log('Dados das Criptomoedas: ', results); // Exibe os resultados das criptomoedas
                                 }
-                                exibirMenu();
+                                exibirMenu(); // Menu após a consulta
                             });
                             break;
                         default:
-                            console.log('Opção inválida. Tente novamente...');
-                            verAtivos(); // Chama verAtivos novamente para permitir ao usuário tentar outra opção.
+                            console.log('Opção inválida. Tente novamente...'); // Mensagem para opção inválida
+                            verAtivos(); // Permite ao usuário tentar novamente
                             break;
                     }
                 });
                 break;
             case '2': // Renda Fixa
+                // Pergunta qual indexador de renda fixa o usuário deseja visualizar
                 rl.question('1. Selic\n2. IPCA\n3. CDI\nEscolha qual o indexador da renda fixa que vocÊ deseja visualizar: ', function(opcao) {
                     switch (opcao) {
                         case '1':
-                            const querySelic = 'SELECT * FROM selic';
+                            const querySelic = 'SELECT * FROM selic'; // Consulta para Selic
                             db.query(querySelic, (err, results) => {
                                 if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message);
+                                    console.log('Erro ao puxar dados: ', err.message); // Erro (se houver)
                                 } else {
-                                    console.log('Dados de Selic: ', results);
+                                    console.log('Dados de Selic: ', results); // Exibe os resultados da Selic
                                 }
-                                exibirMenu();
+                                exibirMenu(); // Menu após a consulta
                             });
                             break;
                         case '2':
-                            const queryIpca = 'SELECT * FROM ipca';
+                            const queryIpca = 'SELECT * FROM ipca'; // Consulta para IPCA
                             db.query(queryIpca, (err, results) => {
                                 if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message);
+                                    console.log('Erro ao puxar dados: ', err.message); // Erro (se houver)
                                 } else {
-                                    console.log('Dados de IPCA: ', results);
+                                    console.log('Dados de IPCA: ', results); // Exibe os resultados do IPCA
                                 }
-                                exibirMenu();
+                                exibirMenu(); // Menu apos consulta
                             });
                             break;
                         case '3':
-                            const queryCdi = 'SELECT * FROM cdi';
+                            const queryCdi = 'SELECT * FROM cdi'; // Consulta para CDI
                             db.query(queryCdi, (err, results) => {
                                 if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message)
+                                    console.log('Erro ao puxar dados: ', err.message) // Erro (se houver)
                                 } else {
-                                    console.log('Dados de CDI: ', results)
+                                    console.log('Dados de CDI: ', results) // Exibe os resultados do CDI
                                 }
-                                exibirMenu();
+                                exibirMenu(); // Menu após consulta
                             });
                             break;
                         default:
-                            console.log('Opção inválida. Tente novamente...')
-                            verAtivos(); // Chama verAtivos novamente para permitir ao usuário tentar outra opção.
+                            console.log('Opção inválida. Tente novamente...') // Mensagem para opção inválida
+                            verAtivos(); // Permite ao usuário tentar novamente
                             break;
                     }
                 });
                 break;
             default:
-                console.log('Opção inválida. Tente novamente...');
-                verAtivos(); // Chama verAtivos novamente para permitir ao usuário tentar outra opção.
+                console.log('Opção inválida. Tente novamente...'); // Mensagem para opção inválida
+                verAtivos(); // Permite ao usuário tentar novamente
                 break;
         } 
     });
 };
 
+// Exporta a função verAtivos para uso em outros módulos
 module.exports = {
     verAtivos
 };
-=======
-'use strict';
-
-const db = require('./database.js');
-const rl = require('./readline.js');
-
-const verAtivos = function() {
-    rl.question('1. Renda Variável\n2. Renda Fixa\nQual investimento você quer ver: ', function(opcao) {
-        switch (opcao) {
-            case '1': // Renda Variável
-                rl.question('1. Ação Brasileira\n2. Fundo Imobiliário\n3. Ação Americana (BDR)\n4. Criptoativos\nEscolha o ativo que você deseja visualizar: ', function(opcao) {
-                    switch (opcao) {
-                        case '1':
-                            const queryAcao = 'SELECT * FROM acoes';
-                            db.query(queryAcao, (err, results) => {
-                                if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message);
-                                } else {
-                                    console.log('Dados das ações:', results);
-                                }
-                                exibirMenu();
-                            });
-                            break;
-                        case '2':
-                            const queryFii = 'SELECT * FROM fii';
-                            db.query(queryFii, (err, results) => {
-                                if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message)
-                                } else {
-                                    console.log('Dados dos Fundos Imobiliários:', results);
-                                }
-                                exibirMenu();
-                            });
-                            break;
-                        case '3':
-                            const queryBdr = 'SELECT * FROM bdr';
-                            db.query(queryBdr, (err, results) => {
-                                if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message);
-                                } else {
-                                    console.log('Dados das Ações americanas: ', results);
-                                }
-                                exibirMenu();
-                            });
-                            break;
-                        case '4':
-                            const queryCripto = 'SELECT * FROM cripto';
-                            db.query(queryCripto, (err, results) => {
-                                if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message);
-                                } else {
-                                    console.log('Dados das Criptomoedas: ', results);
-                                }
-                                exibirMenu();
-                            });
-                            break;
-                        default:
-                            console.log('Opção inválida. Tente novamente...');
-                            verAtivos(); // Chama verAtivos novamente para permitir ao usuário tentar outra opção.
-                            break;
-                    }
-                });
-                break;
-            case '2': // Renda Fixa
-                rl.question('1. Selic\n2. IPCA\n3. CDI\nEscolha qual o indexador da renda fixa que vocÊ deseja visualizar: ', function(opcao) {
-                    switch (opcao) {
-                        case '1':
-                            const querySelic = 'SELECT * FROM selic';
-                            db.query(querySelic, (err, results) => {
-                                if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message);
-                                } else {
-                                    console.log('Dados de Selic: ', results);
-                                }
-                                exibirMenu();
-                            });
-                            break;
-                        case '2':
-                            const queryIpca = 'SELECT * FROM ipca';
-                            db.query(queryIpca, (err, results) => {
-                                if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message);
-                                } else {
-                                    console.log('Dados de IPCA: ', results);
-                                }
-                                exibirMenu();
-                            });
-                            break;
-                        case '3':
-                            const queryCdi = 'SELECT * FROM cdi';
-                            db.query(queryCdi, (err, results) => {
-                                if (err) {
-                                    console.log('Erro ao puxar dados: ', err.message)
-                                } else {
-                                    console.log('Dados de CDI: ', results)
-                                }
-                                exibirMenu();
-                            });
-                            break;
-                        default:
-                            console.log('Opção inválida. Tente novamente...')
-                            verAtivos(); // Chama verAtivos novamente para permitir ao usuário tentar outra opção.
-                            break;
-                    }
-                });
-                break;
-            default:
-                console.log('Opção inválida. Tente novamente...');
-                verAtivos(); // Chama verAtivos novamente para permitir ao usuário tentar outra opção.
-                break;
-        } 
-    });
-};
-
-module.exports = {
-    verAtivos
-};
->>>>>>> 95948d1ff2e2edca0caabe406ab39b442224d489
