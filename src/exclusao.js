1037267cff3cf573d1da963d44c03126bb915b70
Leonardo Peron Krause <102702934+LeonardoPeronKrause@users.noreply.github.com>
2024-09-03 11:03:41 -0300

    Add files via upload

--- src/exclusao.js
+++ src/exclusao.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const { exibirMenu } = require('./edicao');
 const rl = require('./readline');
+const db = require('./database');
 
-function exibirMenuExclusao() {
+function exibirMenu() {
     console.log('=== Menu de Exclusão ===');
     console.log('1. Excluir Ação Brasileira');
     console.log('2. Excluir Fundo Imobiliário (FII)');
@@ -39,23 +39,138 @@ function exibirMenuExclusao() {
                 break;
             case '8':
                 console.log('Até logo, Investidor!');
+                rl.close();
                 break;
             default:
                 console.log('Opção inválida. Tente novamente.');
-                ExibirMenuExclusao();
+                exibirMenu();
                 break;
         }
     });
 } 
 
 function exclusaoRendaVariavel(tabela, tipoAtivo) {
+    const query = `SELECT * FROM ${tabela}`;
 
+    db.query(query, (err, results) => {
+        if (err) {
+            console.log(`Erro ao buscar dados de ${tipoAtivo}!`, err.message);
+            return exibirMenu();
+        }
+
+        if (results.length === 0) {
+            console.log(`Nenhum(a) ${tipoAtivo} cadastrado até o momento!`);
+            return exibirMenu();
+        }
+
+        console.log(`${tipoAtivo} disponíveis para edição:`);
+        results.forEach((ativo, index) => {
+            console.log(`${index + 1} '${ativo.nome}' (${ativo.ticker}) Preço Médio: ${ativo.pm} Setor: ${ativo.setor} Quantidade: ${ativo.quantidade}`);
+        });
+        excluirAtivoVariavelSelecionado(tabela, tipoAtivo, results);
+    });
 }
 
 function exclusaoRendaFixa(tabela, tipoAtivo) {
+    const query = `SELECT * FROM ${tabela}`;
 
+    db.query(query, (err, results) => {
+        if (err) {
+            console.log(`Erro ao buscar dados de ${tipoAtivo}`);
+            return exibirMenu();
+        }
+
+        if (results.length === 0) {
+            console.log(`Nenhum ativo de ${tipoAtivo} cadastrado até o momento!`);
+            return exibirMenu();
+        }
+
+        console.log(`Ativos de ${tipoAtivo} disponíveis para exclusão:`);
+        results.forEach((ativo, index) => {
+            console.log(`${index + 1}. '${ativo.nome}' Preço Médio: ${ativo.pm} Vencimento: ${ativo.vencimento} Quantidade de Cotas: ${ativo.quantidade} Taxa de Juros: ${ativo.taxaJuros}`);
+        });
+        excluirAtivoFixoSelecionado(tabela, tipoAtivo, results);
+    });
+}
+
+function excluirAtivoVariavelSelecionado(tabela, tipoAtivo, results) {
+    rl.question('Qual o número do ativo que você deseja excluir? ', function(numero) {
+        if (numero.trim() === '') {
+            console.log('Nenhum ativo excluído!');
+            return exibirMenu();
+        }
+
+        const index = parseInt(numero) - 1;
+
+        if (index >= 0 && index < results.length) {
+            const ativoSelecionado = results[index];
+
+            rl.question(`Você tem certeza que deseja excluir o ativo ${ativoSelecionado.nome}? [Use = S para sim/N para não]: `, function(confirmacao) {
+                if (confirmacao.toUpperCase() === 'S') {
+                    const deleteQuery = `DELETE FROM ${tabela} WHERE id = ?`;
+                    db.query(deleteQuery, [ativoSelecionado.id], (err) => {
+                        if (err) {
+                            console.log('Erro ao excluir o ativo!', err.message);
+                        } else {
+                            console.log(`O investimento no ativo ${ativoSelecionado.nome} foi excluído com sucesso!`);
+                        }
+                        exibirMenu();
+                    });
+                } else if (confirmacao.toUpperCase() === 'N') {
+                    console.log('Nenhum ativo excluído!');
+                    exibirMenu();
+                } else {
+                    console.log('Você digitou algo diferente de "S" para sim ou "N" para não');
+                    exibirMenu();
+                }
+            });
+        } else {
+            console.log('Número inválido.');
+            exibirMenu();
+        }
+    });
+}
+
+function excluirAtivoFixoSelecionado(tabela, tipoAtivo, results) {
+    rl.question('Qual o número do ativo que você deseja excluir? ', function(numero) {
+        if (numero.trim() === '') {
+            console.log('Nenhum ativo excluído!');
+            return exibirMenu();
+        }
+
+        const index = parseInt(numero) - 1;
+
+        if (index >= 0 && index < results.length) {
+            const ativoSelecionado = results[index];
+
+            rl.question(`Você tem certeza que deseja excluir o investimento ${ativoSelecionado.nome} de ${tipoAtivo}? [Use S para sim/N para não]: `, function(confirmacao) {
+                if (confirmacao.toUpperCase() === 'S') {
+                    const deleteQuery = `DELETE FROM ${tabela} WHERE ID = ?`;
+                    db.query(deleteQuery, [ativoSelecionado.id], (err) => {
+                        if (err) {
+                            console.log('Erro ao excluir o ativo!', err.message);
+                        } else {
+                            console.log(`O investimento ${ativoSelecionado.nome} em ${tabela} foi excluúdo com sucesso!`);
+                        }
+                        exibirMenu();
+                    });
+                } else if (confirmacao.toUpperCase() === 'N') {
+                    console.log('Nenhum ativo excluído!');
+                    exibirMenu();
+                } else {
+                    console.log('Nenhum ativo excluído!');
+                    exibirMenu();
+                }
+            });
+        } else {
+            console.log('Número inválido.');
+            exibirMenu();
+        }
+    });
 }
 
 module.exports = {
-    excluirAtivo
+    excluirAtivoFixoSelecionado,
+    excluirAtivoVariavelSelecionado, 
+    exibirMenu
 }
--- src/readline.js
+++ src/readline.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -7,13 +6,3 @@ const rl = readline.createInterface({
 });
 
 module.exports = rl;
-=======
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-module.exports = rl;
->>>>>>> 95948d1ff2e2edca0caabe406ab39b442224d489
